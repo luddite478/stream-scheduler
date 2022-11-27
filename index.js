7b2b9297a4116524eb2829bf63ca1b34275edf86
User <luddite478@gmail.com>
2022-11-27 19:05:35 +0400

    fix variables

--- index.js
+++ index.js
@@ -207,7 +207,7 @@ function merge_page_media_files(audio_files, video_files, params, output_path) {
 			dflt_image = path.join('images/default', dflt_image)
 
 			const aac_audio = audio_reencode_aac(src_audio)
-			const looped_audio = loop_audio(src_audio, a_repeats)
+			const looped_audio = loop_audio(aac_audio, a_repeats)
 			params = { ...params, resolution }
 			merge_audio_and_image(looped_audio, dflt_image, params, output_path)
 			fs.unlinkSync(looped_audio)
